a86988a4fbc15aa22d50741925a6385273777eb2
siobhanlgorman <siobhanlgorman@gmail.com>
2021-09-01 07:57:32 +0000

    Fix js error for new Date function in base.html

--- run.py
+++ run.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", page_title="Index")
+    return render_template("index.html")
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html", page_title="About")
+    return render_template("about.html", page_title="About", list_of_numbers=[1, 2, 3])
 
 
 @app.route("/contact")
